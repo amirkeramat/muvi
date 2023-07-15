ff7980f87cb094d380eeb3850608c78c716fd69a
Amirkaramat <amir_karamat@yahoo.com>
2023-07-15 18:48:08 +0330

    Create search.jsx

---
+++ src/components/layout/search/Search.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./search.style"
+
+const Search = () => {
+  return (
+    <Container>
+        <input type="text" placeholder="Search for a movie or tv show..." />
+    </Container>
+  )
+}
+
+export default Search
---
+++ src/components/layout/search/search.style.js
@@ -0,0 +1,5 @@
+import tw from "tailwind-styled-components";
+
+export const Container = tw.section`
+ max-h-[400px] bg-red-200 flex w-full
+`;
