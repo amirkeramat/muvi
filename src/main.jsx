032d4c3758fed6fdad6e98ce173a7009e9a8fcf6
Amirkaramat <amir_karamat@yahoo.com>
2023-07-15 18:49:38 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+    </Provider>
+  </HashRouter>
+);
