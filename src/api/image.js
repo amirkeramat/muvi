7769fb837991f362ac372edca3393e3595c798bc
Amirkaramat <amir_karamat@yahoo.com>
2023-07-15 18:45:49 +0330

    add image size url

---
+++ src/api/image.js
@@ -0,0 +1,30 @@
+export const BACK_DROP_W300 = "https://image.tmdb.org/t/p/w300";
+export const BACK_DROP_W780 = "https://image.tmdb.org/t/p/W780";
+export const BACK_DROP_W1280 = "https://image.tmdb.org/t/p/w1280";
+export const BACK_DROP_ORIGINAL = "https://image.tmdb.org/t/p/original";
+
+export const LOGO_W45 = "https://image.tmdb.org/t/p/w45";
+export const LOGO_W92 = "https://image.tmdb.org/t/p/w92";
+export const LOGO_W154 = "https://image.tmdb.org/t/p/w154";
+export const LOGO_W185 = "https://image.tmdb.org/t/p/w185";
+export const LOGO_W300 = "https://image.tmdb.org/t/p/w300";
+export const LOGO_W500 = "https://image.tmdb.org/t/p/w500";
+export const LOGO_ORIGINAL = "https://image.tmdb.org/t/p/original";
+
+export const POSTER_W92 = "https://image.tmdb.org/t/p/w92";
+export const POSTER_W154 = "https://image.tmdb.org/t/p/w154";
+export const POSTER_W185 = "https://image.tmdb.org/t/p/w185";
+export const POSTER_W342 = "https://image.tmdb.org/t/p/w342";
+export const POSTER_W500 = "https://image.tmdb.org/t/p/w500";
+export const POSTER_W780 = "https://image.tmdb.org/t/p/w780";
+export const POSTER_ORIGINAL = "https://image.tmdb.org/t/p/original";
+
+export const PROFILE_W92 = "https://image.tmdb.org/t/p/w45";
+export const PROFILE_W154 = "https://image.tmdb.org/t/p/w185";
+export const PROFILE_W185 = "https://image.tmdb.org/t/p/h632";
+export const PROFILE_ORIGINAL = "https://image.tmdb.org/t/p/original";
+
+export const STILL_W92 = "https://image.tmdb.org/t/p/w92";
+export const STILL_W185 = "https://image.tmdb.org/t/p/w185";
+export const STILL_W300 = "https://image.tmdb.org/t/p/w300";
+export const STILL_ORIGINAL = "https://image.tmdb.org/t/p/original";
