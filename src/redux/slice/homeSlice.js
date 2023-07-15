52ee90a6a8ce05207ee328e98bebbaa102f6adbe
Amirkaramat <amir_karamat@yahoo.com>
2023-07-15 18:50:42 +0330

    Create homeSlice.js

---
+++ src/redux/slice/homeSlice.js
@@ -0,0 +1,58 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BASE_URL, API_KEY } from "../../api/index";
+const initialState = {
+  loading:"idle",
+  data: {},
+  error:null
+};
+
+const options = (type, list) => {
+  return {
+    method: "GET",
+    url: `${BASE_URL}/${type}/${list}`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+};
+
+export const getData = createAsyncThunk(
+  "home/getNowPlaying",
+  async (arg) => {
+    const { type, list } = arg;
+    return await axios
+      .request(options(type, list))
+      .then((res) => res.data)
+      .catch((err) => err.message);
+  }
+);
+const homeSlice = createSlice({
+  name: "homeSlice",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.data = {};
+      state.loading = "idle";
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getData.pending, (state) => {
+      state.loading = "pending";
+    });
+    builder.addCase(getData.fulfilled, (state, action) => {
+      state.loading = "fulfilled";
+      state.data[action.meta.arg.list] = action.payload;
+      state.data[action.meta.arg.list].type = action.meta.arg.type;
+      state.data[action.meta.arg.list].loading = "fulfilled";
+    });
+    builder.addCase(getData.rejected, (state, action) => {
+      state.loading = "rejected";
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default homeSlice.reducer;
+export const resetAction = homeSlice.actions.reset;
