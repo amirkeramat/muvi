9ec78b26b622e1dfdcedbb314d7eb6e080bd89f0
Amirkaramat <amir_karamat@yahoo.com>
2023-07-18 16:32:18 +0330

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getLocation());
-    return ()=>dispatch(resetAction())
+    return () => dispatch(resetAction());
   }, []);
 
   const ipAddress = useSelector((state) => state.ipAddress);
