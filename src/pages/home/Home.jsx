e860b84a88d7046849e4d6aacd69962c8647d75b
Amirkaramat <amir_karamat@yahoo.com>
2023-07-18 09:16:04 +0330

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,10 @@
 import { Hero } from "../../components/home";
-import { useDispatch, useSelector } from "react-redux";
-import { getData, resetAction } from "../../redux/slice/homeSlice";
-import { useEffect } from "react";
 const Home = () => {
-  const homeState = useSelector((state) => state.home);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const arg = {
-      type: "movie",
-      list: "now_playing",
-    };
-    dispatch(getData(arg));
-    return () => dispatch(resetAction());
-  }, []);
-
-  return <div className="w-full h-full">{homeState.loading === "fulfilled" ? <Hero /> : null}</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
