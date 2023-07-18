4d60da5255ec9e612eb186856aaa744f2738e3a1
Amirkaramat <amir_karamat@yahoo.com>
2023-07-18 16:34:39 +0330

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,20 @@
-import { Hero } from "../../components/home";
+import { Hero, CardSlider } from "../../components/home";
 const Home = () => {
   return (
     <>
       <Hero />
+      <div id="movie">
+        <CardSlider type="movie" list="upcoming" />
+        <CardSlider type="movie" list="now_playing" />
+        <CardSlider type="movie" list="popular" />
+        <CardSlider type="movie" list="top_rated" />
+      </div>
+      <div id="tv">
+        <CardSlider type="tv" list="airing_today" />
+        <CardSlider type="tv" list="on_the_air" />
+        <CardSlider type="tv" list="popular" />
+        <CardSlider type="tv" list="top_rated" />
+      </div>
     </>
   );
 };
