27e60b1c6d23628144f2f21a40714fcab9b6d711
Amirkaramat <amir_karamat@yahoo.com>
2023-07-19 22:25:42 +0330

    Update hooks index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,4 @@
-export {default as useHomeState} from "./useState/useHomeState"
+export { default as useHomeState } from "./useState/useHomeState";
+export { default as useDataState } from "./useState/useDataState";
+export { default as useFetch } from "./useFetch";
+export { default as useFetchById } from "./useFetchById";
