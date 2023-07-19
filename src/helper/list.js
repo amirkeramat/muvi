3683c24db9d8aac9bf8de6a5fa6e0a9297b7c439
Amirkaramat <amir_karamat@yahoo.com>
2023-07-19 22:25:53 +0330

    Update helper list.js

--- src/helper/list.js
+++ src/helper/list.js
@@ -3,6 +3,10 @@ const listFixer = (list) => {
     return "NowPlaying";
   } else if (list === "top_rated") {
     return "TopRated";
+  } else if (list === "on_the_air") {
+    return "OnTheAir";
+  } else if (list === "airing_today") {
+    return "AiringToday";
   } else {
     return list.toUpperCase();
   }
