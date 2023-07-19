52b2abd7eb422f90316b53eebdd458009b80f299
Amirkaramat <amir_karamat@yahoo.com>
2023-07-19 22:24:21 +0330

    Update dataSlice.js

--- src/redux/slice/dataSlice.js
+++ src/redux/slice/dataSlice.js
@@ -2,15 +2,24 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { BASE_URL, API_KEY } from "../../api/index";
 const initialState = {
-  loading: "idle",
-  data: {},
+  data: {
+    movie: {},
+    tv: {},
+    loading: "idle",
+    error: null,
+  },
+  videos: {},
+  similar: {},
   error: null,
 };
 
-const options = (type, id) => {
+const options = (type, id, detail) => {
   return {
     method: "GET",
-    url: `${BASE_URL}/${type}/${id}`,
+    url:
+      detail !== ""
+        ? `${BASE_URL}/${type}/${id}/${detail}`
+        : `${BASE_URL}/${type}/${id}`,
     headers: {
       accept: "application/json",
       Authorization: `Bearer ${API_KEY}`,
@@ -19,34 +28,53 @@ const options = (type, id) => {
 };
 
 export const getData = createAsyncThunk("dataSlice/getData", async (arg) => {
-  const { type, id } = arg;
+  const { type, id, detail } = arg;
   return await axios
-    .request(options(type, id))
+    .request(options(type, id, detail))
     .then((res) => res.data)
     .catch((err) => err.message);
 });
+
 const dataSlice = createSlice({
   name: "dataSlice",
   initialState,
   reducers: {
     reset: (state) => {
       state.data = {};
+      state.similar = {};
+      state.videos = {};
       state.loading = "idle";
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(getData.pending, (state) => {
-      state.loading = "pending";
+    builder.addCase(getData.pending, (state, action) => {
+      if (action.meta.arg.detail !== "") {
+        state[action.meta.arg.detail] = "pending";
+      } else {
+        state.data.loading = "pending";
+      }
     });
     builder.addCase(getData.fulfilled, (state, action) => {
-      state.loading = "fulfilled";
-      state.data[action.meta.arg.type] = action.payload;
-      state.data[action.meta.arg.type].type = action.meta.arg.type;
-      state.data[action.meta.arg.type].loading = "fulfilled";
+      if (action.meta.arg.detail !== "") {
+        state[action.meta.arg.detail] = action.payload;
+        state[action.meta.arg.detail].type = action.meta.arg.type;
+        state[action.meta.arg.detail].loading = "fulfilled";
+      } else {
+        state.data.loading = "fulfilled";
+        state.data[action.meta.arg.type] = action.payload;
+        state.data[action.meta.arg.type].type = action.meta.arg.type;
+        state.data[action.meta.arg.type].loading = "fulfilled";
+      }
     });
     builder.addCase(getData.rejected, (state, action) => {
-      state.loading = "rejected";
-      state.error = action.error.message;
+      if (action.meta.arg.detail !== "") {
+        state[action.meta.arg.detail].loading = "rejected";
+        state[action.meta.arg.detail].error = action.error.message;
+      } else {
+        state.data.loading = "rejected";
+        state.data[action.meta.arg.type].loading = "rejected";
+        state.data[action.meta.arg.type].error = action.error.message;
+      }
     });
   },
 });
