643c02a88d019e663b1e77f8d93df24c360cd8ab
Amirkaramat <amir_karamat@yahoo.com>
2023-07-19 22:23:42 +0330

    Update index.js in slice folder

--- src/redux/slice/index.js
+++ src/redux/slice/index.js
@@ -1,3 +1,3 @@
 export { default as homeReducer } from "./homeSlice";
 export { default as dataReducer } from "./dataSlice";
-export {default as ipReducer} from "./ipSlice"
+export { default as ipReducer } from "./ipSlice";
