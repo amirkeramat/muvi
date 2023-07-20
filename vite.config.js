bfe651cc7ac3a88c951f252ce12200554a9c4c1d
Amirkaramat <amir_karamat@yahoo.com>
2023-07-20 12:49:21 +0330

    fix Intro.jsx

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/muvi/"
-})
+  server: {
+    port: 3000,
+    host: true,
+  },
+  base: "/muvi/",
+});
