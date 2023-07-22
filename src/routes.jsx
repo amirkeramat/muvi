26b6a61ea12ba59f206edafbabcad7dd2d3a0884
Amirkaramat <amir_karamat@yahoo.com>
2023-07-23 00:05:17 +0330

    add Search page to router.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,9 @@
-import { Home, MediaInfo } from "./pages";
+import { Home, MediaInfo, Search } from "./pages";
 
 const routes = [
   { path: "/", element: <Home /> },
   { path: "/mediaInfo/:type/:typeId", element: <MediaInfo /> },
+  { path: "/search/:type/:searchValue/:pageNumber", element: <Search /> },
 ];
 
 export default routes;
