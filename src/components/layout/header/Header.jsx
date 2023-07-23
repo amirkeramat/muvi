c5d732634d8176e3068e4672b006e21885a56dbf
Amirkaramat <amir_karamat@yahoo.com>
2023-07-23 20:07:22 +0330

    add dropdown menu

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,7 +1,15 @@
-import { NavLink } from "react-router-dom";
-import { Container, Img, Menu, Nav, SearchIcon } from "./header.style";
-import {Search} from '../index'
+import { NavLink, Link } from "react-router-dom";
+import { Container, Img, Menu, Nav, DropDownMenu } from "./header.style";
+import { CloseOutlined, MenuOutlined } from "@ant-design/icons";
+import { Search } from "../index";
+import { useState } from "react";
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+  if (toggle) {
+    window.document.body.style.overflowY = "hidden";
+  } else {
+    window.document.body.style.overflowY = "scroll";
+  }
   return (
     <Container>
       <Nav>
@@ -30,9 +38,63 @@ const Header = () => {
           >
             Sign Up
           </NavLink>
-          <Search/>
+          <NavLink
+            className={({ isActive }) => isActive && "text-orange-400"}
+            to={"/TvSeries"}
+          >
+            Advance Search
+          </NavLink>
+          <Search />
         </Menu>
-        <Img src="./logo.png" />
+        <i
+          className="z-[9999] text-2xl block lg:hidden"
+          onClick={() => setToggle((prv) => !prv)}
+        >
+          {toggle ? <CloseOutlined /> : <MenuOutlined />}
+        </i>
+        <DropDownMenu onBlur={() => setToggle(false)} $toggle={toggle}>
+          <Search />
+
+          <NavLink
+            className={({ isActive }) => isActive && "text-orange-400"}
+            to={"/"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => isActive && "text-orange-400"}
+            to={"/movies"}
+          >
+            Movies
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => isActive && "text-orange-400"}
+            to={"/TvSeries"}
+          >
+            TvSeries
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => isActive && "text-orange-400"}
+            to={"/TvSeries"}
+          >
+            Sign Up
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => isActive && "text-orange-400"}
+            to={"/TvSeries"}
+          >
+            Advance Search
+          </NavLink>
+        </DropDownMenu>
+        {toggle && (
+          <div
+            onClick={() => setToggle(false)}
+            className="absolute z-[89]  right-0 top-0 h-screen w-full block md:hidden bg-gray-900/20"
+          ></div>
+        )}
+        <Link to={"/"}>
+          <Img src="./logo.png" />
+        </Link>
       </Nav>
     </Container>
   );
--- src/components/layout/header/header.style.js
+++ src/components/layout/header/header.style.js
@@ -13,11 +13,17 @@ export const Nav = tw.nav`
 `;
 
 export const Menu = tw.div`
-hidden items-center md:flex
+hidden items-center lg:flex
 child:pe-8 child:whitespace-nowrap child-hover:text-orange-400
 
 `;
 
 export const SearchIcon = tw.i`
 flex items-center justify-center text-xl cursor-pointer
+`;
+
+export const DropDownMenu = tw.div`
+    fixed inset-0 h-screen bg-gray-950 flex flex-col items-start lg:hidden z-[90]
+     ${(p) => (p.$toggle ? "max-w-[70vw] opacity-100" : "max-w-0 opacity-0")}
+     pt-[60px] child:pb-5 px-4
 `;
